21ddb15c9592284a411cdb48c1917d99709ef7be
Jules - Silamir <jrobin@silamir.com>
2025-06-30 16:23:48 +0200

    Update streamlit_app.py
    add df from api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())    
+# st.text(smoothiefroot_response.json())    
+sf_df = st.dataframe(data=smoothie_froot_response.json(),use_container_width=True)
 
 
 
