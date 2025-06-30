aba5b75dcc01143f42e245efa2de80483753d604
Jules - Silamir <jrobin@silamir.com>
2025-06-30 16:32:00 +0200

    Update streamlit_app.py
    add nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
     # st.write(ingredients_string)
     time_to_insert = st.button('Submit Order')
@@ -38,9 +41,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())    
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
 
 
